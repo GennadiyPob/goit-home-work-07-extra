ba3eedff676f5983ca7f54a867b99a2abc14f102
GennadiyPob <137322270+GennadiyPob@users.noreply.github.com>
2023-09-10 16:41:56 +0300

    Update

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='clean_folder',
-      version='1.0',
+      version='0.0.1',
       description='Clean Folder Tool',
       url='https://github.com/GennadiyPob/goit-home-work-07-extra/clean_folder',
       author='Gennadiy Pobereznichenko',
       author_email='gennadiy.pob@gmail.com',
+      license='MIT'
       packages=['clean_folder'],
       entry_points={'console_scripts': ['clean_folder=clean_folder.clean:main']}
 )
